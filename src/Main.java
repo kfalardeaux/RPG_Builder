6472be264ad2927e900e2dd4855b4cc4dffe6c47
kfalardeaux <katherinefalardeaux@gmail.com>
2025-01-21 11:31:59 -0600

    test 2 of syncing with GitHub; program given basic intro loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // creating a loop that will keep going until user decides to quit program
+        boolean end = false;
+        while(!end){
+            System.out.println("Hello!  Welcome to the project! \n Want to play? Yes or no?  ");
+
+            Scanner scan = new Scanner(System.in);
+            String answer = scan.nextLine();
+            if (answer.equalsIgnoreCase("yes")){
+                System.out.println(" Awesome! ");
+            }
+            else if (answer.equalsIgnoreCase("no")){
+                System.out.println(" Got it!  See ya! ");
+                end = true;
+            }
+            else {
+                System.out.println(" Sorry?  I didn't get what you said. ");
+            }
+
+        }
+
+        System.out.println(" Thanks for playing; come back soon! ");
+
     }
 }
